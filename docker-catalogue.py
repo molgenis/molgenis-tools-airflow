3ae258e75b389865aa68cf3e1af64dc1740a8729
Sido Haakma <sido@haakma.org>
2021-08-27 16:28:14 +0200

    feat: changed to kubernetespod

--- docker-catalogue.py
+++ docker-catalogue.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
-from airflow.operators.docker_operator import DockerOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 import logging
 import sys
 
@@ -16,29 +16,34 @@ default_args = {
 'retries'               : 1,
 'retry_delay'           : timedelta(minutes=5)
 }
+
+init_environments = [
+  k8s.V1EnvVar(name='MG_CATALOGUE_NETWORKS', value='LifeCycle, ATHLETE, LongITools'), 
+  k8s.V1EnvVar(name='MG_CATALOGUE_COHORTS', value='NFBC1966, NFBC1986, KANC'),
+  k8s.V1EnvVar(name='MG_CATALOGUE_URL_STAGING', value='https://data-catalogue-staging.molgeniscloud.org/'),
+  k8s.V1EnvVar(name='MG_CATALOGUE_USERNAME_STAGING', value='admin'),
+  k8s.V1EnvVar(name='MG_CATALOGUE_PASSWORD_STAGING', value=''),
+  k8s.V1EnvVar(name='MG_CATALOGUE_URL_PROD', value='https://emx2.test.molgenis.org/'),
+  k8s.V1EnvVar(name='MG_CATALOGUE_USERNAME_PROD', value='admin'),
+  k8s.V1EnvVar(name='MG_CATALOGUE_PASSWORD_PROD', value='')
+]
+
 with DAG(dag_id='docker_dag', default_args=default_args, catchup=False) as dag:
     t1 = BashOperator(
-    task_id='print_current_date',
-    bash_command='date'
+      task_id='print_current_date',
+      bash_command='date'
     )
-    t2 = DockerOperator(
-    task_id='docker_command',
-    image='molgenis/molgenis-py-catalogue-transform:1.0.15',
-    api_version='auto',
-    auto_remove=True,
-    # command="./run.py",
-    # docker_url="tcp://docker-proxy:2375",
-    # network_mode="bridge",
-    tty=True,
-    # xcom_all=True,
-    environment={ 'MG_CATALOGUE_NETWORKS':'LifeCycle, ATHLETE, LongITools',
-      'MG_CATALOGUE_COHORTS':'NFBC1966, NFBC1986, KANC',
-      'MG_CATALOGUE_URL_STAGING':'https://data-catalogue-staging.molgeniscloud.org/',
-      'MG_CATALOGUE_USERNAME_STAGING':'admin',
-      'MG_CATALOGUE_PASSWORD_STAGING':'',
-      'MG_CATALOGUE_URL_PROD':'https://emx2.test.molgenis.org/',
-      'MG_CATALOGUE_USERNAME_PROD':'admin',
-      'MG_CATALOGUE_PASSWORD_PROD':''}
+    t2 = KubernetesPodOperator(namespace='default',
+      task_id="transfrom-job",
+      namespace='airflow',
+      image="molgenis/molgenis-py-catalogue-transform:1.0.15",
+      # cmds=["Python","-c"],
+      # arguments=["print('hello world')"],
+      # labels={"foo": "bar"},
+      env=init_environments,
+      name="transform-job",
+      get_logs=True,
+      dag=dag
     )
     t3 = BashOperator(
     task_id='print_hello',
