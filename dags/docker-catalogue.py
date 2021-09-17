46988e60bf6711b49d4dd18eb4f4214478c288f9
Sido Haakma <sido@haakma.org>
2021-09-17 09:40:43 +0200

    chore: add defaults and more overridable keys

--- dags/docker-catalogue.py
+++ dags/docker-catalogue.py
@@ -18,14 +18,14 @@ default_args = {
 }
 
 init_environments = [
-  k8s.V1EnvVar(name='MG_CATALOGUE_NETWORKS', value='LifeCycle, ATHLETE, LongITools'), 
-  k8s.V1EnvVar(name='MG_CATALOGUE_COHORTS', value='NFBC1966, NFBC1986, KANC'),
-  k8s.V1EnvVar(name='MG_CATALOGUE_URL_STAGING', value='https://data-catalogue-staging.molgeniscloud.org/'),
-  k8s.V1EnvVar(name='MG_CATALOGUE_USERNAME_STAGING', value="{{ dag_run.conf.staging_username }}"),
-  k8s.V1EnvVar(name='MG_CATALOGUE_PASSWORD_STAGING', value="{{ dag_run.conf.staging_password }}"),
-  k8s.V1EnvVar(name='MG_CATALOGUE_URL_PROD', value='https://emx2.test.molgenis.org/'),
-  k8s.V1EnvVar(name='MG_CATALOGUE_USERNAME_PROD', value="{{ dag_run.conf.catalogue_username }}"),
-  k8s.V1EnvVar(name='MG_CATALOGUE_PASSWORD_PROD', value="{{ dag_run.conf.catalogue_password }}")
+  k8s.V1EnvVar(name='MG_CATALOGUE_NETWORKS', value="{{ dag_run.conf.networks | default('LifeCycle, ATHLETE, LongITools', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_COHORTS', value="{{ dag_run.conf.cohorts | default('NFBC1966, NFBC1986, KANC', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_URL_STAGING', value="{{ dag_run.conf.staging_url | default('https://data-catalogue-staging.gcc.rug.nl/', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_USERNAME_STAGING', value="{{ dag_run.conf.staging_username | default('admin', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_PASSWORD_STAGING', value="{{ dag_run.conf.staging_password | default('xxxx', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_URL_PROD', value="{{ dag_run.conf.prod_url | default('https://data-catalogue.molgeniscloud.org/', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_USERNAME_PROD', value="{{ dag_run.conf.catalogue_username | default('admin', true) }}"),
+  k8s.V1EnvVar(name='MG_CATALOGUE_PASSWORD_PROD', value="{{ dag_run.conf.catalogue_password | default('xxxx', true) }}")
 ]
 
 with DAG(dag_id='catalogue-transform_dag', default_args=default_args, catchup=False) as dag:
